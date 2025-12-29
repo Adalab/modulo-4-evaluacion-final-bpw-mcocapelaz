8a7808b5f6dc5f315fbb4bc2cbf5453bfe8cf2b5
Monica Coca <mcocapelaz@gmail.com>
2025-12-29 11:39:42 +0100

    env file and section to configure the connection to the database added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-
+const mysql = require("mysql2/promise");
 const app = express();
 
 app.use(cors());
@@ -10,3 +11,15 @@ const port = 3000;
 app.listen(port, () => {
   console.log(`Uh! El servidor ya está arrancado: <http://localhost:${port}/>`);
 });
+
+const getConnection = async () => {
+  const datosConexion = {
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+  };
+  const conn = await mysql.createConnection(datosConexion);
+  await conn.connect();
+  return conn;
+};
