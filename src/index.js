8719b6f9334a7424231b717d72a6b7ea90d8d6bd
Monica Coca <mcocapelaz@gmail.com>
2025-12-29 12:33:23 +0100

    route and PUT endpoint to update an entry in the main entity books

--- src/index.js
+++ src/index.js
@@ -45,27 +45,59 @@ app.get("/libros", async (req, res) => {
 });
 
 app.post("/libros", async (req, res) => {
-    try {
-      const conn = await getConnection();
-      const { titulo, autora, estado, genero } = req.body;
-      const queryInsertarLibro =
-        "INSERT INTO libros (titulo, autora, estado, genero) VALUES (?, ?, ?, ?)";
-  
-      const [resultados] = await conn.query(queryInsertarLibro, [
-        titulo,
-        autora,
-        estado,
-        genero,
-      ]);
-  
-      await conn.end();
-  
-      res.status(201).json({
-        message: "Libro creado",
-        id: resultados.insertId,
-      });
-    } catch (err) {
-      console.error("Error al insertar el libro:", err);
-      res.status(500).json({ error: "Error al crear el libro" });
+  try {
+    const conn = await getConnection();
+    const { titulo, autora, estado, genero } = req.body;
+    const queryInsertarLibro =
+      "INSERT INTO libros (titulo, autora, estado, genero) VALUES (?, ?, ?, ?)";
+
+    const [resultados] = await conn.query(queryInsertarLibro, [
+      titulo,
+      autora,
+      estado,
+      genero,
+    ]);
+
+    await conn.end();
+
+    res.status(201).json({
+      message: "Libro creado",
+      id: resultados.insertId,
+    });
+  } catch (err) {
+    console.error("Error al insertar el libro:", err);
+    res.status(500).json({ error: "Error al crear el libro" });
+  }
+});
+
+app.put("/libros/:id_libros", async (req, res) => {
+  try {
+    const conn = await getConnection();
+    const libroId = req.params.id_libros;
+    const { titulo, autora, estado, genero } = req.body;
+    const queryActualizarLibro =
+      "UPDATE libros SET titulo=?, autora=?, estado=?, genero=? WHERE id_libros=?";
+
+    const [resultados] = await conn.query(queryActualizarLibro, [
+      titulo,
+      autora,
+      estado,
+      genero,
+      libroId,
+    ]);
+
+    await conn.end();
+
+    if (resultados.affectedRows === 0) {
+      res.status(404).json({ error: "Libro no encontrado" });
+      return;
     }
-  });
+
+    res.status(200).json({
+      message: "Libro actualizado",
+    });
+  } catch (err) {
+    console.error("Error al actualizar el libro:", err);
+    res.status(500).json({ error: "Error al actualizar el libro" });
+  }
+});
